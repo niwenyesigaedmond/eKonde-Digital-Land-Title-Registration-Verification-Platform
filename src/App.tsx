3018fa32290622788fa488d38434d24f92e6ff1c
Osbert Kamugisha <kamugishaosbert7@gmail.com>
2025-12-12 07:55:14 +0300

    bb

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/eKonde-Digital-Land-Title-Registration-Verification-Platform">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/login" element={<Login />} />
